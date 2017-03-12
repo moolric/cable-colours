274f622f90739b3b94f9c9d15112ca151b1afc7a
Kim Ulrick <kim@mettro.com.au>
2017-03-12 09:58:33 +0000

    removes jquery dependance

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
 (function() {
+    // all the elements
+    const go = document.body.querySelector('.go')
+    const tube = document.body.querySelector('#tube');
+    const fiber = document.body.querySelector('#fiber');
+    const tubeStripe = document.body.querySelector('#tube-stripe');
+    const fiberStripe = document.body.querySelector('#fiber-stripe');
+    const tubeColourText = document.body.querySelector('#tube-colour .colour-name');
+    const fiberColourText = document.body.querySelector('#fiber-colour .colour-name');
+    const tubeStripeText = document.body.querySelector('#tube-colour .colour-stripe');
+    const fiberStripeText = document.body.querySelector('#fiber-colour .colour-stripe');
+    
+    // getting the data object with all the numbers in it
     loadJSON(function(response) {
         // Parse JSON string into object
         var colourData = JSON.parse(response);
-        $('.go').on('click', function() {
-            var capacity = parseInt($('.input-capacity select').val()),
-                number = parseInt($('.input-number input').val()),
+        go.addEventListener('click', function() {
+            var capacity = parseInt(document.body.querySelector('.input-capacity select').value),
+                number = parseInt(document.body.querySelector('.input-number input').value),
                 result;
 
             console.log('capacity', capacity);
@@ -19,72 +31,77 @@
             else {
                 result = filterColours(colourData, capacity, number);
                 if (result) {
-                    $(this).hide();
+                    this.style.display = 'none';
                     loadResult(result);
                 }
             }
         });
-        $('.inputs').on('keypress', 'input', function() {
-            clearResults();
-        }).on('change', 'select', function() {
-            clearResults();
-        });;
+        inputs = document.body.querySelectorAll('.inputs');
+        inputs.forEach(function(el) {
+            el.addEventListener('click', function() {
+                clearResults();
+            });
+        });
     });
+    
+    function loadJSON(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', 'data.json', true); 
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+    
+    function filterColours(colourData, capacity, number) {
+        var filterObj = colourData.filter(function(e) {
+            return e.capacity == capacity && e.number == number;
+        });
+        if (!filterObj.length) {
+            return null;
+    
+        }
+        return filterObj[0];
+    }
+    
+    function loadResult(result) {
+        tube.classList = 'colour-' + result.tube;
+        fiber.classList = 'colour-' + result.fiber;
+        tubeStripe.classList = 'stripes stripes-' + result.tube_stripe;
+        fiberStripe.classList = 'stripes stripes-' + result.fiber_stripe;
+    
+        tubeColourText.textContent = result.tube;
+        fiberColourText.textContent = result.fiber;
+        if (result.tube_stripe) {
+            tubeStripeText.textContent = result.tube_stripe + " stripe";
+        }
+        else {
+            tubeStripeText.textContent = '';
+        }
+        if (result.fiber_stripe) {
+            fiberStripeText.textContent = result.fiber_stripe + " stripe";
+        }
+        else {
+            fiberStripeText.textContent = '';
+        }
+    
+    }
+    
+    function clearResults() {
+        go.style.display = 'block';
+        tubeColourText.textContent = '';
+        tubeStripeText.textContent = '';
+        fiberColourText.textContent = '';
+        fiberStripeText.textContent = '';
+    }
+    
+    
 })();
 
 
-function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-function filterColours(colourData, capacity, number) {
-    var filterObj = colourData.filter(function(e) {
-        return e.capacity == capacity && e.number == number;
-    });
-    if (!filterObj.length) {
-        return null;
-
-    }
-    return filterObj[0];
-}
-
-function loadResult(result) {
-    $('#tube').removeClass().addClass('colour-' + result.tube);
-    $('#fiber').removeClass().addClass('colour-' + result.fiber);
-    $('#tube-stripe').removeClass().addClass('stripes stripes-' + result.tube_stripe);
-    $('#fiber-stripe').removeClass().addClass('stripes stripes-' + result.fiber_stripe);
-
-    $('#tube-colour .colour-name').text(result.tube);
-    $('#fiber-colour .colour-name').text(result.fiber);
-    if (result.tube_stripe) {
-        $('#tube-colour .colour-stripe').text(result.tube_stripe + " stripe");
-    }
-    else {
-        $('#tube-colour .colour-stripe').text('');
-    }
-    if (result.fiber_stripe) {
-        $('#fiber-colour .colour-stripe').text(result.fiber_stripe + " stripe");
-    }
-    else {
-        $('#fiber-colour .colour-stripe').text('');
-    }
-
-}
-
-function clearResults() {
-    $('.go').show();
-    $('#tube-colour .colour-name').text('');
-    $('#fiber-colour .colour-name').text('');
-    $('#tube-colour .colour-stripe').text('');
-    $('#fiber-colour .colour-stripe').text('');
-}
